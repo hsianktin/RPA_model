0ce63fe90f1bd3d3c372a1636af20baa59a652ba
Xiangting Li <xiangting.li@ucla.edu>
2023-05-19 19:20:14 -0700

    release

--- evaluate_diffusion.jl
+++ evaluate_diffusion.jl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 A = [i/10 for i in 5:1:20]
 include("evaluate_base_diffusion.jl")
 using ProgressMeter
@@ -54,61 +53,4 @@ else
     close(f)
 ## work completed
     CSV.write("./data_simu/diff_$(exp_label)_$simu_label.csv",df)
-=======
-A = [i/10 for i in 5:5:20]
-include("evaluate_base_diffusion.jl")
-using ProgressMeter
-
-if length(ARGS) == 1
-    exp_label = ""
-    simu_label = ARGS[1]
-    # simu_label = "wt_15mM_salt"
-elseif length(ARGS) == 2
-    exp_label=ARGS[1]
-    println("exp_label=$exp_label")
-    simu_label=ARGS[2]
-    println("simu_label=$simu_label")
-else
-    exp_label = "wt_15mM_salt"
-    simu_label = "perturb_diffusion_1240"
-end
-
-if exp_label== "general" # self-citing
-    run(`julia evaluate.jl wt_15mM_salt $simu_label`)
-    run(`julia evaluate.jl wt_150mM_salt $simu_label`)
-else
-    initialize(exp_label,simu_label)
-    length_of_paras = length(index_p.k_on)
-    @showprogress 1 "analyzing $exp_label..." for i in 1:length_of_paras
-        local k_on,k_off,v_open,v_close,D1 = index_p[i,:]
-        evaluate(df,k_on,k_off,v_open,v_close,D1,folds)
-        # @printf("analyzing progress: %.2f\r",i/length_of_paras)
-    end
-    # diff contains all the information needed
-
-    analyze(df,"k_on")
-    analyze(df,"k_off")
-    analyze(df,"v_open")
-    analyze(df,"v_close")
-    analyze(df,"D1")
-    analyze(df,"α")
-    analyze(df,"β")
-    # CSV.write("./data_simu/analyze_$(exp_label)_$(simu_label).csv")
-    minval,index_min = findmin(df.diff)
-    println("minval = $minval")
-    k_on,k_off,v_open,v_close,D1,α,β,d=df[index_min,:]
-    println(@sprintf("k_on=%.1E,k_off=%.1E,v_open=%.1E,v_close=%.1E,α=%.2f,β=%.2f",k_on,k_off,v_open,v_close,α,β))
-    # k_on,k_off,v_open,v_close,α,β= 1e-5,1e-4,1e-2,1e-3,1,2
-    ensemble_plot(k_on,k_off,v_open,v_close,D1,folds,α,β)
-    yaxis!(:flip)
-    ylims!(-2.0,2.0)
-    # xlims!(1500,2400)
-    savefig("./figs/rsa_state_transition_$(exp_label)_$simu_label.svg")
-    microstates_plot(k_on,k_off,v_open,v_close,D1,folds)
-    f = open("./figs/sources/loss_$(exp_label)_$(simu_label).csv","w")
-    write(f,"$minval")
-    close(f)
-## work completed
-CSV.write("./data_simu/diff_$(exp_label)_$simu_label.csv",df)
->>>>>>> cd1b2244bd3b8da94c96088688359f38b8af2f05
 end
--- initialize.jl
+++ initialize.jl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # initialize necessary environment
 println("initializing modules...")
 push!(LOAD_PATH,pwd())
@@ -62,61 +61,3 @@ catch
 end
 
 println("initialization completed")
-=======
-# initialize necessary environment
-println("initializing modules...")
-push!(LOAD_PATH,pwd())
-using TonksGaswithReactionGaps
-try
-    using Statistics
-    using Random
-    using CSV
-    using Plots
-    using DataFrames
-    using Printf
-    using ProgressMeter
-    using Distributed
-    using DelimitedFiles
-catch
-    println("dependency requirements are not met. Try installing packages...")
-    using Pkg
-    Pkg.add("Statistics")
-    Pkg.add("CSV")
-    Pkg.add("Plots")
-    Pkg.add("DataFrames")
-    Pkg.add("ProgressMeter")
-    Pkg.add("DelimitedFiles")
-    using Statistics
-    using Random
-    using CSV
-    using Plots
-    using DataFrames
-    using Printf
-    using ProgressMeter
-    using Distributed
-    using DelimitedFiles
-end
-println("module initialized")
-println("initializing folders...")
-try 
-    mkdir("data_simu")
-catch
-    println("data_simu/ already exists")
-end
-
-try
-    mkdir("data_exp")
-catch
-    println("data_exp/ already exists")
-end
-
-try
-    mkdir("figs")
-    mkdir("figs/landscape")
-    mkdir("figs/sources")
-catch
-    println("figs/ already exists")
-end
-
-println("initialization completed")
->>>>>>> cd1b2244bd3b8da94c96088688359f38b8af2f05
